682b252299f13891115bc27af4893999de34716c
kw <xz835.525o39@yahoo.com>
2022-04-10 14:09:38 +0900

    commit

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,10 @@
 def fizz_buzz(number)
   if number % 15 == 0
-    "Fizz Buzz"
-  elsif  number % 3 == 0
-    "Fizz"
+    'Fizz Buzz'
+  elsif number % 3 == 0
+    'Fizz'
   elsif number % 5 == 0
-    "Buzz"
+    'Buzz'
   else
     number.to_s
   end
